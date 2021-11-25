6e1233de0b4117cd99e209328785587080422bda
musangisilvia <musangisilvia@gmail.com>
2021-11-25 21:32:56 +0300

    disable no-used-vars

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,7 +3,9 @@ export default function taskBlock(trueOrFalse) {
   const task2 = true;
 
   if (trueOrFalse) {
+    // eslint-disable-next-line no-unused-vars
     const task = true;
+    // eslint-disable-next-line no-unused-vars
     const task2 = false;
   }
 
